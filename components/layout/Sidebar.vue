7c9e62dfe78bc008c40daf40e7b58e5645d04867
Oleh Khmelevskoi <khmelevskoioleh@gmail.com>
2024-11-23 17:46:01 +0100

    first commit

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,20 @@
+<template>
+  <aside class="px-5 py-8 bg-gray-900 h-full relative w-full">
+    <NuxtLink to="/" class="block mb-8">
+      <NuxtImg src="/icon-green.png" width="100" class="mx-auto" />
+    </NuxtLink>
+    <button
+      class="absolute top-5 right-5 transition-colors hover:text-purple-400 text-2xl text-white"
+    >
+      <Icon name="line-md:log-out" size="20" />
+    </button>
+    <UiButton class="bg-blue-900 grid items-center justify-center">
+      Send
+    </UiButton>
+    <slot />
+  </aside>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="grid" style="min-height: 100vh">
+    <aside><LayoutSidebar /></aside>
+
+    <div>
+      <slot />
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+console.log("Loaded Default Layout");
+</script>
+
+<style scoped>
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 6fr;
+}
+</style>
