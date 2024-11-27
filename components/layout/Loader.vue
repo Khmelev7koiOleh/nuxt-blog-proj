55a51d1acb0b9a03969d2bb3a139611a8f0ce3a0
Oleh Khmelevskoi <khmelevskoioleh@gmail.com>
2024-11-27 17:23:19 +0100

    auth was successfuly added and successfuly adapted to netlify one more time to check 2

--- components/layout/Loader.vue
+++ components/layout/Loader.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <img src="/icon-green.png" width="100" class="mx-auto w-full h-screen" />
+    <img
+      src="/icon-green.png"
+      width="100"
+      class="mx-auto w-full h-screen max-w-[50%] max-h-[50%]"
+    />
   </div>
 </template>
 
