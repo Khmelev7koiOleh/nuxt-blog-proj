dfc103131e4735648f1e308934348cf3926e99b1
Oleh Khmelevskoi <khmelevskoioleh@gmail.com>
2024-11-27 17:12:22 +0100

    auth was successfuly added and successfuly adapted to netlify one more time to check

--- pages/login.vue
+++ pages/login.vue
@@ -102,7 +102,7 @@ watch(emailRef, () => {
           v-model="nameRef"
           type="text"
           :placeholder="
-            errorMessages ? `${errorMessages.name}` : 'Enter your name'
+            errorMessages ? `${errorMessages.name}` : 'Enter your name...'
           "
           :class="{
             'placeholder:text-red-500': errorMessages,
@@ -114,7 +114,7 @@ watch(emailRef, () => {
             type="button"
             class="bg-gray-800 text-white rounded hover:bg-gray-700"
             @click="login"
-            >Login</UiButton
+            >Login .</UiButton
           >
           <UiButton
             type="button"
